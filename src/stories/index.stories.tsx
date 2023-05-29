467761e17fde305e15fe63a2222628f5f2254b9c
Arturo Montoya <montoya>
2023-05-29 14:55:35 -0700

    update storybook

--- src/stories/index.stories.tsx
+++ src/stories/index.stories.tsx
@@ -2,7 +2,7 @@ import Editor from "./index";
 import debounce from "lodash/debounce";
 import { Props } from "..";
 import React from "react";
-import { Story, Meta } from "@storybook/react/types-6-0";
+import { Story, Meta } from "@storybook/react";
 
 export default {
   title: "Editor",
@@ -25,7 +25,7 @@ export default {
   },
 } as Meta;
 
-const Template: Story<Props> = args => <Editor {...args} />;
+const Template: Story<Props> = (args) => <Editor {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
@@ -189,7 +189,7 @@ Persisted.args = {
     `# Persisted
   
 The contents of this editor are persisted to local storage on change (edit and reload)`,
-  onChange: debounce(value => {
+  onChange: debounce((value) => {
     const text = value();
     localStorage.setItem("saved", text);
   }, 250),
